a36a47784407db0b50651bda7b3bf0fc0d6db795
Restyled.io <commits@restyled.io>
2024-05-05 11:08:05 +0000

    Restyled by reorder-python-imports

--- fire/parser_test.py
+++ fire/parser_test.py
@@ -11,12 +11,13 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-
 """Tests for the parser module."""
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
 
-from __future__ import absolute_import, division, print_function
-
-from fire import parser, testutils
+from fire import parser
+from fire import testutils
 
 
 class ParserTest(testutils.BaseTestCase):
